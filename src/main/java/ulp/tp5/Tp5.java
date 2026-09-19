f7ce993c7dcef96b67d84b3ee41e40df583a4f13
FedeTrivelli <fede.trivelli@gmail.com>
2026-09-19 00:50:48 -0300

    GUI

---
+++ src/main/java/ulp/tp5/Tp5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ulp.tp5;
+
+/**
+ *
+ * @author FEDE-PC
+ */
+public class Tp5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
